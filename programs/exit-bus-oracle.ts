e7594dea60969144af46eae6f9f05d6a983b772b
George Avsetsin <mail@sjors.ru>
2023-02-19 23:52:24 +0400

    feat: exit requests

--- programs/exit-bus-oracle.ts
+++ programs/exit-bus-oracle.ts
@@ -6,3 +6,22 @@ const oracle = program.command('exit-bus-oracle');
 addAccessControlSubCommands(oracle, exitBusOracleContract);
 addParsingCommands(oracle, exitBusOracleContract);
 addPauseUntilSubCommands(oracle, exitBusOracleContract);
+
+oracle
+  .command('exit-requests')
+  .option('-b, --blocks <number>', 'duration in blocks', '7200')
+  .action(async (options) => {
+    const { blocks } = options;
+    const { number: toBlock } = await exitBusOracleContract.runner.provider.getBlock('latest');
+    const fromBlock = toBlock - Number(blocks);
+
+    const events = await exitBusOracleContract.queryFilter('ValidatorExitRequest', fromBlock, toBlock);
+    const requests = events.map((event) => {
+      if ('args' in event) {
+        const [stakingModuleId, nodeOperatorId, validatorIndex, validatorPubkey, timestamp] = event.args;
+        return { stakingModuleId, nodeOperatorId, validatorIndex, validatorPubkey, timestamp };
+      }
+    });
+
+    console.log('events', requests);
+  });
