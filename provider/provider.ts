33806edd6da42fb8685ff29b934303fd759c4103
George Avsetsin <mail@sjors.ru>
2023-03-14 10:45:12 +0400

    feat: goerli

--- provider/provider.ts
+++ provider/provider.ts
@@ -1,10 +1,7 @@
 import { envs } from '@configs';
 import { JsonRpcProvider, Network } from 'ethers';
 
-const chainId = 1337803;
-const url = envs.EL_API_PROVIDER;
-
-const network = new Network('zhejiang', chainId);
+const network = new Network(envs.EL_NETWORK_NAME, envs.EL_CHAIN_ID);
 const options = { staticNetwork: network, batchMaxCount: 1 };
 
-export const provider = new JsonRpcProvider(url, network, options);
+export const provider = new JsonRpcProvider(envs.EL_API_PROVIDER, network, options);
