eeb3b2c682b61a5ed8145573c006a2ae96c27d91
George Avsetsin <mail@sjors.ru>
2023-03-24 11:55:40 +0400

    feat: tx confirmation

--- utils/tx.ts
+++ utils/tx.ts
@@ -1,8 +1,24 @@
+import prompts from 'prompts';
 import { TransactionResponse } from 'ethers';
+import { wallet } from '@provider';
+import chalk from 'chalk';
 
 export const wrapTx = async (call: () => Promise<TransactionResponse>) => {
+  const network = await wallet.provider.getNetwork();
+  const networkName = chalk.yellow(network.name);
+  const fromAddress = chalk.yellow(wallet.address);
+
+  const { confirm } = await prompts({
+    type: 'confirm',
+    name: 'confirm',
+    message: `Tx will be sent to the ${networkName} network from ${fromAddress}. Continue?`,
+    initial: true,
+  });
+
+  if (!confirm) return;
+
   const tx = await call();
-  console.log('tx sent', tx.hash);
+  console.log('tx sent', chalk.green(tx.hash));
 
   console.log('waiting for tx to be mined...');
   const receipt = await tx.wait();
