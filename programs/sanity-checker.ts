429efae9ce6e5ad483c7267e0d8ed056300ef76e
George Avsetsin <mail@sjors.ru>
2023-04-06 10:37:14 +0400

    feat: setters for sanity checker

--- programs/sanity-checker.ts
+++ programs/sanity-checker.ts
@@ -39,3 +39,19 @@ sanityChecker
   .action(async (limit) => {
     await authorizedCall(sanityCheckerContract, 'setMaxPositiveTokenRebase', [Number(limit)]);
   });
+
+sanityChecker
+  .command('set-max-accounting-extra-data-list-items-count')
+  .description('sets max accounting extra data list items count')
+  .argument('<limit>', 'max extra data list items count')
+  .action(async (limit) => {
+    await authorizedCall(sanityCheckerContract, 'setMaxAccountingExtraDataListItemsCount', [Number(limit)]);
+  });
+
+sanityChecker
+  .command('set-max-node-operators-per-extra-data-item-count')
+  .description('sets max node operators per extra data item count')
+  .argument('<limit>', 'max node operators per extra data item count')
+  .action(async (limit) => {
+    await authorizedCall(sanityCheckerContract, 'setMaxNodeOperatorsPerExtraDataItemCount', [Number(limit)]);
+  });
