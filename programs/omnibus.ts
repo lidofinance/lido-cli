07399e03cb3794086fd503af78cad350c8e3f399
George Avsetsin <mail@sjors.ru>
2024-04-22 17:58:53 +0300

    feat: omnibus script runner

--- programs/omnibus.ts
+++ programs/omnibus.ts
@@ -1,5 +1,5 @@
 import { program } from '@command';
-import { checkTmCanForward, forwardVoteFromTm, printTx } from '@utils';
+import { checkTmCanForward, forwardVoteFromTm, logger, printTx } from '@utils';
 import { printVoteTxData, promptVoting } from './omnibus/';
 import { tmContract } from '@contracts';
 
@@ -28,4 +28,18 @@ omnibus
 
     await printVoteTxData(voteTxData);
     await forwardVoteFromTm(voteTxData.newVoteCalldata);
+  });
+
+omnibus
+  .command('script')
+  .argument('<script>', ' script to run')
+  .action(async (script) => {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const scripts = require(`./omnibus-scripts`);
+
+    if (typeof scripts[script] === 'function') {
+      await scripts[script]();
+    } else {
+      logger.error(`Script ${script} not found`);
+    }
   });
