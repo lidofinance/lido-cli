021fc8b45a43afdba00d05c9fee0b019ac6ddaa4
Alexander Movsunov <alex.mov@lido.fi>
2026-02-12 19:27:18 +0400

    fix: gas limit

--- utils/voting.ts
+++ utils/voting.ts
@@ -52,7 +52,7 @@ export const voteAgainst = async (voteId: number) => {
 };
 
 export const executeVote = async (voteId: number) => {
-  const result = await contractCallTx(votingContract, 'executeVote', [voteId, { gasLimit: 20_000_000n }]);
+  const result = await contractCallTx(votingContract, 'executeVote', [voteId, { gasLimit: 16_000_000n }]);
   logger.success('Vote executed');
   return result;
 };
