c304c1cc1f0f101eee7051bab84ae91832e55180
madlabman <10616301+madlabman@users.noreply.github.com>
2026-04-29 18:53:27 +0000

    feat: add steth option to permissionless operator creation

--- programs/common/staking-modules.ts
+++ programs/common/staking-modules.ts
@@ -1,5 +1,7 @@
-import { Contract } from 'ethers';
+import { Contract, Signature } from 'ethers';
+import { wallet } from '@providers';
 import { DepositData, contractCallTxWithConfirm, joinHex, supplementAndVerifyDepositDataArray } from '@utils';
+import permitAbi from 'abi/StETHPermit.json';
 
 type PermissionlessEntryOptions = {
   accountingContract: Contract;
@@ -11,6 +13,38 @@ type PermissionlessEntryOptions = {
   rewardAddress: string;
   extendedManagerPermissions: boolean;
   referrer: string;
+};
+
+const PERMIT_VALUE_BUFFER = 10n;
+const PERMIT_DEADLINE = 2n ** 256n - 1n;
+
+const buildPermit = async (tokenAddress: string, spender: string, value: bigint) => {
+  const token = new Contract(tokenAddress, permitAbi, wallet);
+  const [name, nonce, network] = await Promise.all([
+    token.name(),
+    token.nonces(wallet.address),
+    wallet.provider?.getNetwork(),
+  ]);
+
+  if (!network) throw new Error('No provider available for permit signing');
+
+  const signature = Signature.from(
+    await wallet.signTypedData(
+      { name, version: '2', chainId: network.chainId, verifyingContract: tokenAddress },
+      {
+        Permit: [
+          { name: 'owner', type: 'address' },
+          { name: 'spender', type: 'address' },
+          { name: 'value', type: 'uint256' },
+          { name: 'nonce', type: 'uint256' },
+          { name: 'deadline', type: 'uint256' },
+        ],
+      },
+      { owner: wallet.address, spender, value, nonce, deadline: PERMIT_DEADLINE },
+    ),
+  );
+
+  return [value, PERMIT_DEADLINE, signature.v, signature.r, signature.s] as const;
 };
 
 export const addPermissionlessNodeOperatorETH = async ({
@@ -46,6 +80,51 @@ export const addPermissionlessNodeOperatorETHFromFile = async (
   await supplementAndVerifyDepositDataArray(depositData);
 
   await addPermissionlessNodeOperatorETH({
+    ...options,
+    keysCount: depositData.length,
+    publicKeys: joinHex(depositData.map(({ pubkey }) => pubkey)),
+    signatures: joinHex(depositData.map(({ signature }) => signature)),
+  });
+};
+
+export const addPermissionlessNodeOperatorStETH = async ({
+  accountingContract,
+  permissionlessGateContract,
+  keysCount,
+  publicKeys,
+  signatures,
+  managerAddress,
+  rewardAddress,
+  extendedManagerPermissions,
+  referrer,
+}: PermissionlessEntryOptions) => {
+  const curveId = await accountingContract.DEFAULT_BOND_CURVE_ID();
+  const value = await accountingContract['getBondAmountByKeysCount(uint256,uint256)'](keysCount, curveId);
+  const permit = await buildPermit(
+    await accountingContract.LIDO(),
+    await accountingContract.getAddress(),
+    value + PERMIT_VALUE_BUFFER,
+  );
+
+  await contractCallTxWithConfirm(permissionlessGateContract, 'addNodeOperatorStETH', [
+    keysCount,
+    publicKeys,
+    signatures,
+    [managerAddress, rewardAddress, !!extendedManagerPermissions],
+    permit,
+    referrer,
+  ]);
+};
+
+export const addPermissionlessNodeOperatorStETHFromFile = async (
+  filePath: string,
+  options: Omit<PermissionlessEntryOptions, 'keysCount' | 'publicKeys' | 'signatures'>,
+) => {
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  const depositData: DepositData[] = require(filePath);
+  await supplementAndVerifyDepositDataArray(depositData);
+
+  await addPermissionlessNodeOperatorStETH({
     ...options,
     keysCount: depositData.length,
     publicKeys: joinHex(depositData.map(({ pubkey }) => pubkey)),
--- programs/csm.ts
+++ programs/csm.ts
@@ -7,6 +7,8 @@ import {
   addPauseUntilSubCommands,
   addPermissionlessNodeOperatorETH,
   addPermissionlessNodeOperatorETHFromFile,
+  addPermissionlessNodeOperatorStETH,
+  addPermissionlessNodeOperatorStETHFromFile,
 } from './common';
 import {
   contractCallTxWithConfirm,
@@ -82,6 +84,33 @@ csm
   });
 
 csm
+  .command('add-operator-steth')
+  .description('adds node operator using stETH bond')
+  .option('-k, --keys-count <number>', 'keys count', '1')
+  .option('-p, --public-keys <string>', 'public keys')
+  .option('-s, --signatures <string>', 'signatures')
+  .option('-m, --manager-address <string>', 'manager address', wallet.address)
+  .option('-a, --reward-address <string>', 'reward address', wallet.address)
+  .option('-e, --extended-manager-permissions', 'extended manager permissions', false)
+  .option('-r, --referrer <string>', 'referrer', ZeroAddress)
+  .action(async (options) => {
+    const { keysCount, publicKeys, signatures, managerAddress, rewardAddress, extendedManagerPermissions, referrer } =
+      options;
+
+    await addPermissionlessNodeOperatorStETH({
+      accountingContract: csAccountingContract,
+      permissionlessGateContract,
+      keysCount,
+      publicKeys,
+      signatures,
+      managerAddress,
+      rewardAddress,
+      extendedManagerPermissions,
+      referrer,
+    });
+  });
+
+csm
   .command('add-operator-with-keys-from-file')
   .description('adds node operator with keys from file')
   .argument('<file-path>', 'file path')
@@ -93,6 +122,27 @@ csm
     const { managerAddress, rewardAddress, extendedManagerPermissions, referrer } = options;
 
     await addPermissionlessNodeOperatorETHFromFile(filePath, {
+      accountingContract: csAccountingContract,
+      permissionlessGateContract,
+      managerAddress,
+      rewardAddress,
+      extendedManagerPermissions,
+      referrer,
+    });
+  });
+
+csm
+  .command('add-operator-with-keys-from-file-steth')
+  .description('adds node operator with keys from file using stETH bond')
+  .argument('<file-path>', 'file path')
+  .option('-m, --manager-address <string>', 'manager address', wallet.address)
+  .option('-a, --reward-address <string>', 'reward address', wallet.address)
+  .option('-e, --extended-manager-permissions', 'extended manager permissions', false)
+  .option('-r, --referrer <string>', 'referrer', ZeroAddress)
+  .action(async (filePath, options) => {
+    const { managerAddress, rewardAddress, extendedManagerPermissions, referrer } = options;
+
+    await addPermissionlessNodeOperatorStETHFromFile(filePath, {
       accountingContract: csAccountingContract,
       permissionlessGateContract,
       managerAddress,
